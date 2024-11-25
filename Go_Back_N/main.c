70d655dd6dfc2c6f709e5a940196ad7d274db861
حبيبة مصطفى محمود قناوى <143437900+Habibakenawy@users.noreply.github.com>
2024-11-25 17:42:52 +0200

    Create main.c

---
+++ Go_Back_N/main.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+#include "protocol5.h"
+
+int main() {
+    char data[]="A B C D E F G H I J K L M N O P Q";
+    char result[MAX_PKT];
+    protocol5(data, result);
+    printf("Result: %s\n", result);
+    return 0;
+
+}
